6fefc5faab33015f5284d36b9521da861f6ba7f4
Albina <albina.nikiforova@optimics.cz>
2023-10-28 11:32:36 +0200

    Binary search

---
+++ learning-c/binsearch/exc3-1/index.c
@@ -0,0 +1,41 @@
+#include "stdio.h"
+#include "time.h"
+
+int binsearch(int x, int v[], int n);
+
+int main() {
+  clock_t start, end;
+  double cpu_time_used;
+
+  start = clock();
+
+  int val = 4;
+  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+  printf("%d\n", binsearch(val, arr, 10));
+  end = clock();
+  cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+  printf("%f\n", cpu_time_used);
+
+  return 0;
+}
+
+int binsearch(int x, int v[], int n) {
+  int low, high, mid;
+
+  low = 0;
+  high = n - 1;
+  mid = (low + high) / 2;
+  while (low <= high && x != v[mid]) {
+    if (x < v[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+    mid = (low + high) / 2;
+  }
+  if (x == v[mid])
+    return mid;
+  else
+    return -1;
+}
---
+++ learning-c/binsearch/exc3-1/readme.md
@@ -0,0 +1,6 @@
+Exercise 3-1. Our binary search makes two tests inside the loop, when one would suffice (at the price of more tests outside). Write a version with only one test inside the loop and measure the difference in run-time.
+
+Difference in run-time:
+
+First: 0.000029
+Second:  0.000054
